0cc6597b0d1e56ebac35945d37b8b579764373d1
Sethisfir <micka_8@hotmail.fr>
2017-01-10 11:40:44 +0100

    Exercice_3

---
+++ index.js
@@ -0,0 +1,5 @@
+	const SEUIL= 200;
+
+for (var nbCase = 1; nbCase<=SEUIL; nbCase++) {
+	document.getElementById("conteneur").innerHTML += "<li>"+nbCase+"</li>";
+}
---
+++ javascript.js
@@ -0,0 +1,6 @@
+	const SEUIL= 200;
+	var nbCase;
+
+for (var nbCase = 1; SEUIL; nbCase++) {
+	document.getElementByClass("conteneur").innerHTML = "<li>"+nbCase+"</li>";
+}
